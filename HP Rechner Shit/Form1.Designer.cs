4e16300a58fa2b8e1bdba9b89e1ecc9ad4a621f9
MrDj200 <mrdj201@gmail.com>
2018-09-23 04:28:36 +0200

    Fixed Missing comic sans ms ♥

--- HP Rechner Shit/Form1.Designer.cs
+++ HP Rechner Shit/Form1.Designer.cs
@@ -63,7 +63,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(83, 4);
+            this.label1.Location = new System.Drawing.Point(111, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 20);
             this.label1.TabIndex = 2;
@@ -73,7 +74,8 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(50, 30);
+            this.label2.Location = new System.Drawing.Point(67, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 20);
             this.label2.TabIndex = 4;
@@ -83,7 +85,8 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(69, 53);
+            this.label3.Location = new System.Drawing.Point(92, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 20);
             this.label3.TabIndex = 6;
@@ -93,7 +96,8 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(17, 97);
+            this.label4.Location = new System.Drawing.Point(23, 134);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(145, 20);
             this.label4.TabIndex = 8;
@@ -102,7 +106,9 @@
             // numericUpDown1
             // 
             this.numericUpDown1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.numericUpDown1.Location = new System.Drawing.Point(176, 4);
+            this.numericUpDown1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(235, 6);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             -727379969,
             232,
@@ -114,7 +120,7 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 24);
             this.numericUpDown1.TabIndex = 9;
             this.numericUpDown1.ThousandsSeparator = true;
             this.numericUpDown1.Value = new decimal(new int[] {
@@ -126,7 +132,9 @@
             // numericUpDown2
             // 
             this.numericUpDown2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.numericUpDown2.Location = new System.Drawing.Point(176, 30);
+            this.numericUpDown2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(235, 42);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             -727379969,
             232,
@@ -138,7 +146,7 @@
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(160, 24);
             this.numericUpDown2.TabIndex = 10;
             this.numericUpDown2.ThousandsSeparator = true;
             this.numericUpDown2.Value = new decimal(new int[] {
@@ -150,28 +158,32 @@
             // numericUpDown3
             // 
             this.numericUpDown3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.numericUpDown3.Location = new System.Drawing.Point(176, 56);
+            this.numericUpDown3.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(235, 78);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown3.Maximum = new decimal(new int[] {
             -727379969,
             232,
             0,
             0});
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.Size = new System.Drawing.Size(160, 24);
             this.numericUpDown3.TabIndex = 11;
             this.numericUpDown3.ThousandsSeparator = true;
             // 
             // numericUpDown4
             // 
             this.numericUpDown4.BackColor = System.Drawing.Color.Green;
-            this.numericUpDown4.Location = new System.Drawing.Point(176, 97);
+            this.numericUpDown4.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown4.Location = new System.Drawing.Point(235, 134);
+            this.numericUpDown4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown4.Maximum = new decimal(new int[] {
             -727379969,
             232,
             0,
             0});
             this.numericUpDown4.Name = "numericUpDown4";
-            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.Size = new System.Drawing.Size(160, 24);
             this.numericUpDown4.TabIndex = 12;
             this.numericUpDown4.ThousandsSeparator = true;
             // 
@@ -179,7 +191,8 @@
             // 
             this.outputLabel1.AutoSize = true;
             this.outputLabel1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel1.Location = new System.Drawing.Point(8, 198);
+            this.outputLabel1.Location = new System.Drawing.Point(11, 274);
+            this.outputLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel1.Name = "outputLabel1";
             this.outputLabel1.Size = new System.Drawing.Size(177, 20);
             this.outputLabel1.TabIndex = 13;
@@ -189,7 +202,8 @@
             // 
             this.outputLabel2.AutoSize = true;
             this.outputLabel2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel2.Location = new System.Drawing.Point(41, 224);
+            this.outputLabel2.Location = new System.Drawing.Point(55, 310);
+            this.outputLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel2.Name = "outputLabel2";
             this.outputLabel2.Size = new System.Drawing.Size(108, 20);
             this.outputLabel2.TabIndex = 14;
@@ -199,7 +213,8 @@
             // 
             this.outputLabel3.AutoSize = true;
             this.outputLabel3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel3.Location = new System.Drawing.Point(13, 276);
+            this.outputLabel3.Location = new System.Drawing.Point(17, 382);
+            this.outputLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel3.Name = "outputLabel3";
             this.outputLabel3.Size = new System.Drawing.Size(172, 20);
             this.outputLabel3.TabIndex = 15;
@@ -209,7 +224,8 @@
             // 
             this.outputLabel4.AutoSize = true;
             this.outputLabel4.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel4.Location = new System.Drawing.Point(6, 307);
+            this.outputLabel4.Location = new System.Drawing.Point(8, 425);
+            this.outputLabel4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel4.Name = "outputLabel4";
             this.outputLabel4.Size = new System.Drawing.Size(179, 20);
             this.outputLabel4.TabIndex = 16;
@@ -217,25 +233,29 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(191, 200);
+            this.textBox1.Location = new System.Drawing.Point(255, 277);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.Size = new System.Drawing.Size(181, 26);
             this.textBox1.TabIndex = 17;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(191, 226);
+            this.textBox2.Location = new System.Drawing.Point(255, 313);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(137, 20);
+            this.textBox2.Size = new System.Drawing.Size(181, 26);
             this.textBox2.TabIndex = 18;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(133, 137);
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(177, 190);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 32);
             this.button1.TabIndex = 19;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
@@ -243,11 +263,12 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 439);
+            this.trackBar1.Location = new System.Drawing.Point(16, 608);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(316, 45);
+            this.trackBar1.Size = new System.Drawing.Size(421, 45);
             this.trackBar1.TabIndex = 20;
             this.trackBar1.TickFrequency = 25;
             this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
@@ -258,7 +279,8 @@
             // 
             this.outputLabel6.AutoSize = true;
             this.outputLabel6.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel6.Location = new System.Drawing.Point(8, 430);
+            this.outputLabel6.Location = new System.Drawing.Point(11, 595);
+            this.outputLabel6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel6.Name = "outputLabel6";
             this.outputLabel6.Size = new System.Drawing.Size(64, 20);
             this.outputLabel6.TabIndex = 21;
@@ -268,7 +290,8 @@
             // 
             this.outputLabel5.AutoSize = true;
             this.outputLabel5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel5.Location = new System.Drawing.Point(2, 337);
+            this.outputLabel5.Location = new System.Drawing.Point(3, 467);
+            this.outputLabel5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel5.Name = "outputLabel5";
             this.outputLabel5.Size = new System.Drawing.Size(183, 20);
             this.outputLabel5.TabIndex = 22;
@@ -276,26 +299,29 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(191, 278);
+            this.textBox3.Location = new System.Drawing.Point(255, 385);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(137, 20);
+            this.textBox3.Size = new System.Drawing.Size(181, 26);
             this.textBox3.TabIndex = 23;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(191, 307);
+            this.textBox4.Location = new System.Drawing.Point(255, 425);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(137, 20);
+            this.textBox4.Size = new System.Drawing.Size(181, 26);
             this.textBox4.TabIndex = 24;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(191, 337);
+            this.textBox5.Location = new System.Drawing.Point(255, 467);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(137, 20);
+            this.textBox5.Size = new System.Drawing.Size(181, 26);
             this.textBox5.TabIndex = 25;
             // 
             // panel1
@@ -312,16 +338,17 @@
             this.panel1.Controls.Add(this.numericUpDown4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(340, 169);
+            this.panel1.Size = new System.Drawing.Size(453, 234);
             this.panel1.TabIndex = 26;
             // 
             // Form1
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 498);
+            this.ClientSize = new System.Drawing.Size(453, 690);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -335,8 +362,10 @@
             this.Controls.Add(this.outputLabel3);
             this.Controls.Add(this.outputLabel2);
             this.Controls.Add(this.outputLabel1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
